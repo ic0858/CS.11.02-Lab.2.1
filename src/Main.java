2b67ecdca5d47af7debe75d5bcb613be0d8fa57b
ic085 <ic0858bai@gmail.com>
2023-09-15 12:55:56 +0800

    lab 2.1 done

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,42 @@ public class Main {
 
     public static void main(String[] args) {
 
+        ;
     }
 
     // 1. add
+    public static int add(int numOne, int numTwo) {
+        return numOne+numTwo;
+    }
     
     // 2. add
+    public static int add(int numOne, int numTwo, int numThree, int numFour) {
+        return add(add(numOne,numTwo),add(numThree,numFour));
+    }
+
 
     // 3. morningGreeting
-
+    public static String morningGreeting(String name) {
+        return "早上好, "+name+"!";
+    }
     // 4. afternoonGreeting
-
+    public static String afternoonGreeting(String name) {
+        return "下午好, "+name+"!";
+    }
     // 5. triple
-
+    public static String triple(String name) {
+        return name.repeat(3);
+    }
     // 6. half
-
+    public static double half(int value) {
+        return value/2.0;
+    }
     // 7. roundPositiveValueToNearestInteger
-
+    public static double roundPositiveValueToNearestInteger(double value) {
+        return (int) (value+0.5);
+    }
     // 8. roundNegativeValueToNearestInteger
-
+    public static double roundNegativeValueToNearestInteger(double value) {
+        return (int)  (value-0.5);
+    }
 }
